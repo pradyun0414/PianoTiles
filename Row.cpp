1f7263eec206c31f444991d3cbb0bc219b925cb0
advaithiyer <advaith.2525@gmail.com>
2024-04-20 03:49:51 -0500

    changed some setters and getters

--- Row.cpp
+++ Row.cpp
@@ -36,6 +36,23 @@ Row::Row(){
 
 }
 
+void Row::initializeRow(uint8_t keyColors, int16_t rowY){
+    rowHeight = 30;
+    onScreen = false;
+    this->keyColors = keyColors;
+    this->rowY = rowY;
+
+
+
+    for(uint8_t i = 0; i < 4; i++){
+        uint8_t adjustedVal = (keyColors >> (3 - i)) & 0x01;
+        if(adjustedVal == 1)
+            keys[i].initializeKey(i * 32, rowY, 32, 30, Key::black_key);
+        else
+            keys[i].initializeKey(i * 32, rowY, 32, 30, Key::white_key);
+    }
+}
+
 void Row::drawRow(){
     for(uint8_t i = 0; i < 4; i++){
         keys[i].drawKey();
@@ -55,12 +72,27 @@ void Row::clearRow(){
     }
 }
 
+void Row::setRowY(int16_t rowY){
+    this->rowY = rowY;
+    for(uint8_t i = 0; i < 4; i++){
+        keys[i].setKeyY(rowY);
+    }
+}
+
+int16_t Row::getRowY(){
+    return rowY;
+}
+
 void Row::setOnScreen(){
     onScreen = true;
 }
 
 void Row::setOffScreen(){
     onScreen = false;
+}
+
+bool Row::getDisplayState(){
+    return onScreen;
 }
 
 Row::~Row()
